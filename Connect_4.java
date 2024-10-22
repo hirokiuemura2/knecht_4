3652a66fa788f5ad9887645e1c9eec8ae11d68f3
Hiroki Uemura <hirokiuemura2@gmail.com>
2024-10-22 10:15:18 +0900

    Added checkScore() function

--- Connect_4.java
+++ Connect_4.java
@@ -1,5 +1,8 @@
 public class Connect_4 {
     public static void main(String[] args) {
-        
+        System.out.println("hello world");
+    }
+    public static int checkScore(int[][] board) {
+        return 0;
     }
 }
