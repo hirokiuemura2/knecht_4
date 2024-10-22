9d272063ec90e08b0c295b086b0c0fcf3242e410
Hiroki Uemura <hirokiuemura2@gmail.com>
2024-10-22 10:23:31 +0900

    Added vertical checking portion to score function

--- Connect_4.java
+++ Connect_4.java
@@ -2,7 +2,30 @@ public class Connect_4 {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    //returns the winner (1, 0, or -1)
     public static int checkScore(int[][] board) {
+        int count = 0;
+        int previousPlayer = 0;
+        //vertical
+        for (int i = 0; i < 7; i++) {
+            previousPlayer = 0;
+            for (int j = 0; j < 6; j++) {
+                if (previousPlayer == board[i][j] || previousPlayer == 0) {
+                    count++;
+                } else {
+                    if (board[i][j] == 0) {
+                        j = 7;
+                    } else {
+                        previousPlayer = board[i][j];
+                        count = 0;
+                    }
+                }
+                if (count == 4) {
+                    return previousPlayer;
+                }
+            }
+        }
+        //horizontal - faydhi
         return 0;
     }
 }
