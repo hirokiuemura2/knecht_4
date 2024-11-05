ba271a7b75cfb4686c4161e3caf6dcc14d675d66
Hiroki Uemura <hirokiuemura2@gmail.com>
2024-11-05 11:58:46 +0900

    started the basic column selection system

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
-// let bigContainer = document.createElement('div');
+let bigContainer = document.createElement('div');
+bigContainer.classList.add('bigContainer');
 const container = document.querySelector('body');
-// container.appendChild(bigContainer);
+container.appendChild(bigContainer);
 // bigContainer.classList.add("bigContainer");
 
 // const body = document.querySelector('body');
@@ -22,24 +23,33 @@ allSquares.forEach(square => {
 //     })
 // }
 
-// let addSquares = (num1, num2) => {
-//     for (let i = 0; i < num1; i++) {
-//         let newContainer = document.createElement('div');
-//         bigContainer.append(newContainer);
-//         newContainer.classList.add('rows');
-//         for (let j = 0; j < num2; j++) {
-//             let squareContainer = document.createElement('div');
-//             newContainer.append(squareContainer);
-//             squareContainer.classList.add('square');
-//             squareContainer.textContent="";
-//             squareContainer.addEventListener('mouseover', () => {
-//                 squareContainer.style.backgroundColor = "red";
-//             });
-//         }
-//     }
-// }
+let addSquares = (num1, num2) => {
+    for (let i = 0; i < num1; i++) {
+        let newContainer = document.createElement('div');
+        bigContainer.append(newContainer);
+        newContainer.classList.add('rowContainer', 'row' + i);
+        for (let j = 0; j < num2; j++) {
+            let squareContainer = document.createElement('div');
+            newContainer.append(squareContainer);
+            squareContainer.classList.add('square');
+            squareContainer.classList.add("col"+ j);
+            squareContainer.textContent="";
+            squareContainer.addEventListener('mouseover', () => {
+                squareContainer.style.backgroundColor = "red";
+            });
+            squareContainer.addEventListener('mouseleave', () => {
+                if (!squareContainer.classList.contains(0)) {
+                    let otherSquare = bigContainer.querySelector('.row5').querySelector('.col'+j);
+                    otherSquare.style.backgroundColor = 'blue';
+                }
+                
+                squareContainer.style.backgroundColor = "white";
+            });
+        }
+    }
+}
 
-// addSquares(6,7);
+addSquares(6,7);
 
 // let sizer = document.querySelector('.sizer');
 // sizer.addEventListener('click',() => {
